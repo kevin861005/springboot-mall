53609b87eaef1d4686fa4f215ee9989ad3f87913
Kevin Chen <kevin861005@gmail.com>
2023-09-03 21:05:31 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- springboot-mall/src/main/java/com/kevin/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/kevin/springbootmall/service/ProductService.java
@@ -3,13 +3,17 @@ package com.kevin.springbootmall.service;
 import com.kevin.springbootmall.dto.ProductRequest;
 import com.kevin.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
-    public Product getProductById(Integer productId);
+    Product getProductById(Integer productId);
 
-    public int createProduct(ProductRequest productRequest);
+    int createProduct(ProductRequest productRequest);
 
-    public void updateProduct(Integer productId, ProductRequest productRequest);
+    void updateProduct(Integer productId, ProductRequest productRequest);
 
-    public void deleteProductById(Integer productId);
+    void deleteProductById(Integer productId);
+
+    List<Product> getProducts();
 }
